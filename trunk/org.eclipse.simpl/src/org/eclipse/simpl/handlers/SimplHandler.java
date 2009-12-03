d9ea49c720f59b4662aef6c701edda30f2275324
hahnml@t-online.de <hahnml@t-online.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2009-12-03 10:35:22 +0000

    Bereitstellung eines Extension-Point Beispiels für SIMPL.

---
+++ trunk/org.eclipse.simpl/src/org/eclipse/simpl/handlers/SimplHandler.java
@@ -0,0 +1,32 @@
+package org.eclipse.simpl.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.simpl.ui.SWTApp;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class SimplHandler extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public SimplHandler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		//IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		new SWTApp(Display.getDefault());
+		return null;
+	}
+}
