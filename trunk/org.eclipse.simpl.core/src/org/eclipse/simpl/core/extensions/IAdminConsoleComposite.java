610fa4528573aa1336de3a9dfcde77ee81c46d6e
hahnml@t-online.de <hahnml@t-online.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2009-12-29 09:16:48 +0000

    Rückgängig machen der Änderungen.

--- trunk/org.eclipse.simpl.core/src/org/eclipse/simpl/core/extensions/IAdminConsoleComposite.java
+++ trunk/org.eclipse.simpl.core/src/org/eclipse/simpl/core/extensions/IAdminConsoleComposite.java
@@ -1,18 +1,16 @@
 package org.eclipse.simpl.core.extensions;
 
-import java.util.LinkedHashMap;
 import org.eclipse.swt.widgets.Composite;
 
 public interface IAdminConsoleComposite {
-	
+
 	public Composite getComposite();
-	
+
 	public void setComposite(Composite composite);
-	
+
 	public void createComposite(Composite composite);
-	
+
 	public void saveSettings(String parentItem, String item, String settingName);
-	
-	public void loadSettings(String parentItem, String item,
-			String settingName);
+
+	public void loadSettings(String parentItem, String item, String settingName);
 }
