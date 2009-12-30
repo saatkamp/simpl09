b297ff888ac45ce7b4fc1cdf55d7dfbca2f0671d
hahnml@t-online.de <hahnml@t-online.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2009-12-30 13:44:06 +0000

    Fertigstellung der Anbindung des BPEL-DM Plug-Ins an den SIMPL Core Client für das Auslesen aller vom SIMPL Core unterstützten Datenquellen zur Laufzeit.

--- trunk/org.eclipse.bpel.simpl.ui/src/org/eclipse/bpel/simpl/ui/properties/DMActivityPropertySection.java
+++ trunk/org.eclipse.bpel.simpl.ui/src/org/eclipse/bpel/simpl/ui/properties/DMActivityPropertySection.java
@@ -253,9 +253,10 @@ public abstract class DMActivityPropertySection extends BPELPropertySection{
 	/**
 	 * Opens a statement editor shell.
 	 */
-	public void openStatementEditor(String eClass){
+	public void openStatementEditor(String eClass, String language){
 		//TODO Auswahl der Sprache muss noch dynamisch erledigt werden
-		new StatementEditor(this, "SQL", eClass);
+		System.out.println("StatementEditor: " + eClass + " : " + language);
+		new StatementEditor(this, language, eClass);
 	}
 	
 	public void setStatement(StatementHashMap statement){
