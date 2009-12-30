1f82f4ed870fda033a2765f18eb907d693d27ddc
hahnml@t-online.de <hahnml@t-online.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2009-12-30 11:35:51 +0000

    Umstrukturierung der PropertySections in mehrere aktivitätsbezogene Klassen zur Reduzierung der Komplexität der Verarbeitungslogik der StatementEditoren.

---
+++ trunk/org.eclipse.bpel.simpl.ui/src/org/eclipse/bpel/simpl/ui/properties/DropPropertySection.java
@@ -0,0 +1,13 @@
+package org.eclipse.bpel.simpl.ui.properties;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class DropPropertySection extends DMActivityPropertySection {
+
+	@Override
+	protected void createClient(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
