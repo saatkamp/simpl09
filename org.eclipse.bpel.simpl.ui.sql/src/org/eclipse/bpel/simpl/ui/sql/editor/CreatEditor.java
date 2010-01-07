647b29bb4c4d0d007d3f10fe5039dae5c267e2de
ferass_z2000 <ferass_z2000@yahoo.com>
2010-01-07 11:24:43 +0000

    Die restliche Editoren anhand der SelectEditor erzeugt.

---
+++ org.eclipse.bpel.simpl.ui.sql/src/org/eclipse/bpel/simpl/ui/sql/editor/CreatEditor.java
@@ -0,0 +1,63 @@
+package org.eclipse.bpel.simpl.ui.sql.editor;
+
+import org.eclipse.bpel.simpl.ui.extensions.AStatementEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+
+public class CreatEditor extends AStatementEditor {
+
+	private Composite comp = null;
+	private Composite compos = null;
+	private StyledText statementText = null;
+	
+	
+	public CreatEditor() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void createComposite(Composite composite) {
+		GridData gridData1 = new GridData();
+		gridData1.horizontalAlignment = GridData.FILL;
+		gridData1.grabExcessHorizontalSpace = true;
+		gridData1.grabExcessVerticalSpace = true;
+		gridData1.verticalAlignment = GridData.FILL;
+		GridData gridData = new GridData();
+		gridData.horizontalAlignment = GridData.FILL;
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.verticalAlignment = GridData.FILL;
+		comp = new Composite(composite, SWT.NONE);
+		comp.setLayout(new GridLayout());
+		GridData gridData2 = new GridData();
+		gridData2.horizontalAlignment = GridData.FILL;
+		gridData2.grabExcessHorizontalSpace = true;
+		gridData2.grabExcessVerticalSpace = true;
+		gridData2.verticalAlignment = GridData.FILL;
+		compos = new Composite(comp, SWT.NONE);
+		compos.setLayout(new GridLayout());
+		compos.setLayoutData(gridData2);
+		comp.setLayoutData(gridData);
+		statementText = new StyledText(comp, SWT.BORDER);
+		statementText.setLayoutData(gridData1);
+		statementText.addModifyListener(new ModifyListener(){
+
+			@Override
+			public void modifyText(ModifyEvent e) {
+				// TODO Auto-generated method stub
+				setStatement(statementText.getText());
+			}});
+		
+		setComposite(comp);
+		
+		if (getStatement()!=null){
+			statementText.setText(getStatement());
+		}
+	}
+
+}
