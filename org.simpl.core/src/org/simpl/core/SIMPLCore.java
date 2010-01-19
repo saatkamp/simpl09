1a7be3177a9bcd8312df62c6d9154e0472b6c9ea
michael.schneidt <michael.schneidt@arcor.de>
2010-01-19 21:49:47 +0000

    - lib und build path für Projekt angepasst, damit das Projekt in Eclipse keine Fehler wirft - Datasource-Plug-Ins in simpl-core-plugins.jar ausgelagert - DatasourceServiceProvider lädt den DatasourceService jetzt über type und subtype einer Datenquelle - simpl-core-config.xml für die Registrierung der Plug-Ins - SIMPLConfig Klasse für das Laden der Einstellungen aus der simpl-core-config.xml - getDatasourceTypes, getDatasourceSubtypes, getDatasourceLanguages implementiert und DatasourceInfo Interface dafür erstellt - SIMPLCore Zugriff geht jetzt über Methoden - simpl-core-deploy.zip aktualisiert (muss wegen Log4J jetzt in Tomcat root verz. entpackt werden)

--- org.simpl.core/src/org/simpl/core/SIMPLCore.java
+++ org.simpl.core/src/org/simpl/core/SIMPLCore.java
@@ -1,30 +1,44 @@
 package org.simpl.core;
 
+import java.util.List;
+
 import org.simpl.core.administration.AdministrationService;
 import org.simpl.core.administration.AdministrationServiceImpl;
+import org.simpl.core.datasource.DatasourceInfo;
+import org.simpl.core.datasource.DatasourceService;
 import org.simpl.core.datasource.DatasourceServiceProvider;
 import org.simpl.core.storage.StorageService;
 import org.simpl.core.storage.StorageServiceImpl;
 
 /**
- * Provides access to all services of the SIMPLCore.
+ * <b>Purpose: Provides access to all services of the SIMPLCore.</b> <br>
+ * <b>Description: This class implements the singleton pattern.</b> <br>
+ * <b>Copyright:</b> <br>
+ * <b>Company:</b> SIMPL<br>
  * 
- * This class implements the singleton pattern.
- * 
- * @author schneimi
- * 
- *         SVN $Id: SIMPLCore.java 451 2009-11-30 14:37:55Z michael.schneidt@arcor.de $
+ * @author Michael Schneidt <michael.schneidt@arcor.de><br>
+ * @version $Id$<br>
+ * @link http://code.google.com/p/simpl09/
  */
-public class SIMPLCore {
+public class SIMPLCore implements DatasourceInfo {
   private static SIMPLCore instance = null;
-  public static AdministrationService administrationService = new AdministrationServiceImpl();
-  public static DatasourceServiceProvider datasourceServiceProvider = new DatasourceServiceProvider();
-  public static StorageService storageService = new StorageServiceImpl();
+  private static SIMPLConfig config = new SIMPLConfig();
+  private static AdministrationService administrationService = new AdministrationServiceImpl();
+  private static DatasourceServiceProvider datasourceServiceProvider = new DatasourceServiceProvider();
+  private static StorageService storageService = new StorageServiceImpl();
+
+  /**
+   * Private constructor for singleton.
+   */
+  private SIMPLCore() {
+    // do nothing
+  }
 
   /**
    * Returns the instance of SIMPLCore.
    * 
    * @return Instance of SIMPLCore.
+   * @throws Exception
    */
   public static synchronized SIMPLCore getInstance() {
     if (SIMPLCore.instance == null) {
@@ -32,5 +46,71 @@ public class SIMPLCore {
     }
 
     return SIMPLCore.instance;
+  }
+
+  /**
+   * Returns the instance of the administration service
+   * 
+   * @return
+   */
+  public AdministrationService administrationService() {
+    return administrationService;
+  }
+
+  /**
+   * Returns the instance of a datasource service dependent on the given type and subtype.
+   * 
+   * @param dsType
+   * @param dsSubtype
+   * @return
+   */
+  public DatasourceService datasourceService(String dsType, String dsSubtype) {
+    return datasourceServiceProvider.getInstance(dsType, dsSubtype);
+  }
+
+  /**
+   * Returns the instance of the storage service.
+   * 
+   * @return
+   */
+  public StorageService storageService() {
+    return storageService;
+  }
+
+  /**
+   * Returns the SIMPL configuration.
+   * 
+   * @return
+   */
+  public SIMPLConfig config() {
+    return config;
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see org.simpl.core.datasource.DatasourceInfo#getDatasourceTypes()
+   */
+  @Override
+  public List<String> getDatasourceTypes() {
+    return datasourceServiceProvider.getDatasourceTypes();
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see org.simpl.core.datasource.DatasourceInfo#getDatasourceSubtypes(java.lang.String)
+   */
+  @Override
+  public List<String> getDatasourceSubtypes(String dsType) {
+    return datasourceServiceProvider.getDatasourceSubtypes(dsType);
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see
+   * org.simpl.core.datasource.DatasourceInfo#getDatasourceLanguages(java.lang.String)
+   */
+  @Override
+  public List<String> getDatasourceLanguages(String dsSubtype) {
+    return datasourceServiceProvider.getDatasourceLanguages(dsSubtype);
   }
 }
--- org.simpl.core/src/org/simpl/core/datasource/DatasourceServiceProvider.java
+++ org.simpl.core/src/org/simpl/core/datasource/DatasourceServiceProvider.java
@@ -1,40 +1,151 @@
 package org.simpl.core.datasource;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
-public class DatasourceServiceProvider {
-  public DatasourceService getInstance(String dsType) { // type = type + subtype (RDBDERBY)
-    DatasourceService datasource = null;
+import org.simpl.core.SIMPLCore;
 
-    try {
-      datasource = (DatasourceService) Class.forName(
-          "org.simpl.core.datasource.plugins." + dsType.toLowerCase() + "." + dsType + "DatasourceService").newInstance();
-    } catch (Exception e) {
-      e.printStackTrace();
+/**
+ * <b>Purpose:</b> <br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b> <br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Schneidt <michael.schneidt@arcor.de><br>
+ * @version $Id$<br>
+ * @link http://code.google.com/p/simpl09/
+ */
+public class DatasourceServiceProvider implements DatasourceInfo {
+  /**
+   * Maps the datasource service types to a list of supporting datasource service plugin
+   * instances.
+   */
+  private HashMap<String, List<DatasourceServicePlugin>> datasourceServices = new HashMap<String, List<DatasourceServicePlugin>>();
+
+  /**
+   * Initialize all datasource service plugins.
+   */
+  public DatasourceServiceProvider() {
+    loadDatasourcePlugins();
+  }
+
+  /**
+   * Returns the datasource service instance that suppots the given datasource type and
+   * subtype.
+   * 
+   * @param dsType
+   * @param dsSubtype
+   * @return
+   */
+  public DatasourceService getInstance(String dsType, String dsSubtype) {
+    DatasourceServicePlugin datasourceServiceInstance = null;
+    List<DatasourceServicePlugin> datasourceServicePlugins = this.datasourceServices
+        .get(dsType);
+    List<String> datasourceServiceSubtypes = null;
+
+    // search for a plugin that supports the given subtype
+    for (DatasourceServicePlugin datasourceServicePluginInstance : datasourceServicePlugins) {
+      datasourceServiceSubtypes = datasourceServicePluginInstance.getDatasourceSubtypes();
+
+      for (String subtype : datasourceServiceSubtypes) {
+        if (subtype.equals(dsSubtype)) {
+          datasourceServiceInstance = datasourceServicePluginInstance;
+        }
+      }
     }
 
-    return datasource;
+    return datasourceServiceInstance;
   }
 
-  public List<String> getDatasourceLanguages(String datasourceSubType) {
-    // TODO Auto-generated method stub
-    return null;
+  /**
+   * Loads instances of all plugins and retrievs the information about types, subtypes and
+   * languages.
+   */
+  private void loadDatasourcePlugins() {
+    List<String> plugins = SIMPLCore.getInstance().config().getDataSourceServicePlugins();
+    Iterator<String> pluginIterator = plugins.iterator();
+    DatasourceServicePlugin datasourceServiceInstance;
+    String datasourceType = null;
+
+    while (pluginIterator.hasNext()) {
+      try {
+        datasourceServiceInstance = (DatasourceServicePlugin) Class.forName(
+            (String) pluginIterator.next()).newInstance();
+        datasourceType = datasourceServiceInstance.getDatasourceType();
+
+        if (datasourceServices.containsKey(datasourceType)) {
+          datasourceServices.get(datasourceType).add(datasourceServiceInstance);
+        } else {
+          List<DatasourceServicePlugin> datasourceServicePluginList = new ArrayList<DatasourceServicePlugin>();
+          datasourceServicePluginList.add(datasourceServiceInstance);
+          datasourceServices.put(datasourceType, datasourceServicePluginList);
+        }
+      } catch (InstantiationException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (ClassNotFoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
   }
 
-  public List<String> getDatasourceSubTypes(String datasourceType) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
+  /*
+   * (non-Javadoc)
+   * @see org.simpl.core.datasource.DatasourceInfo#getDatasourceTypes()
+   */
+  @Override
   public List<String> getDatasourceTypes() {
-    // TODO Auto-generated method stub
-    return null;
+    return new ArrayList<String>(datasourceServices.keySet());
   }
 
-  public ArrayList<String> getSupportedDatasourceTypes() {
-    // TODO Auto-generated method stub
-    return null;
+  /*
+   * (non-Javadoc)
+   * @see org.simpl.core.datasource.DatasourceInfo#getDatasourceSubtypes(java.lang.String)
+   */
+  @Override
+  public List<String> getDatasourceSubtypes(String dsType) {
+    List<String> datasourceSubtypes = new ArrayList<String>();
+
+    for (DatasourceServicePlugin datasourceServicePlugin : datasourceServices.get(dsType)) {
+      for (String datasourceSubtype : datasourceServicePlugin.getDatasourceSubtypes()) {
+        if (!datasourceSubtypes.contains(datasourceSubtype)) {
+          datasourceSubtypes.add(datasourceSubtype);
+        }
+      }
+    }
+
+    return datasourceSubtypes;
   }
 
+  /*
+   * (non-Javadoc)
+   * @see
+   * org.simpl.core.datasource.DatasourceInfo#getDatasourceLanguages(java.lang.String)
+   */
+  @Override
+  public List<String> getDatasourceLanguages(String dsSubtype) {
+    List<String> datasourceLanguages = new ArrayList<String>();
+
+    for (String datasourceType : datasourceServices.keySet()) {
+      for (DatasourceServicePlugin datasourceServicePlugin : datasourceServices
+          .get(datasourceType)) {
+        if (datasourceServicePlugin.getDatasourceLanguages().containsKey(dsSubtype)) {
+          for (String language : datasourceServicePlugin.getDatasourceLanguages().get(
+              dsSubtype)) {
+            if (!datasourceLanguages.contains(language)) {
+              datasourceLanguages.add(language);
+            }
+          }
+        }
+      }
+    }
+
+    return datasourceLanguages;
+  }
 }
