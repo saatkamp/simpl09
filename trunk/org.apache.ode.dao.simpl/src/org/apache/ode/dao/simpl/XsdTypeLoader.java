94354e931e04cf921730c3cc902d404527afffcf
hahnml@t-online.de <hahnml@t-online.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2010-01-26 14:09:17 +0000

    Kleine Änderungen am ODE DAO Projekt.

--- trunk/org.apache.ode.dao.simpl/src/org/apache/ode/dao/simpl/XsdTypeLoader.java
+++ trunk/org.apache.ode.dao.simpl/src/org/apache/ode/dao/simpl/XsdTypeLoader.java
@@ -1,5 +1,8 @@
 package org.apache.ode.dao.simpl;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -9,29 +12,35 @@ import org.apache.log4j.PropertyConfigurator;
 import commonj.sdo.helper.XSDHelper;
 
 public class XsdTypeLoader {
-	
+
 	private static Logger logger = Logger.getLogger(XsdTypeLoader.class);
-	
-	private static final String SIMPL_DAO_XSD = "simpl-dao.xsd";
-	
+
+	private static final String SIMPL_DAO_XSD = System.getProperty("user.dir")
+			+ "/webapps/ode/WEB-INF/conf/simpl-dao.xsd";
+
 	public static void defineSIMPLTypes() {
 		PropertyConfigurator.configure("log4j.properties");
 
-		InputStream is = XsdTypeLoader.class.getResourceAsStream(SIMPL_DAO_XSD);
-		//logger.info("SIMPL XSD FILE: " + ClassLoader.getSystemResource("schema/"+SIMPL_DAO_XSD).getPath());
-		logger.info("SIMPL XSD FILE: " + XsdTypeLoader.class.getResource("/"+SIMPL_DAO_XSD).getPath());
-//		if (is == null) {
-//			System.out.println("InputStream is null");
-//		} else {
-//			System.out.println("Obtained Input Stream from resoruce");
-//		}
-		XSDHelper.INSTANCE.define(is, null);
+		FileInputStream file;
 		try {
-			is.close();
+			file = new FileInputStream(new File(SIMPL_DAO_XSD));
+			InputStream is = file;
+
+			if (is == null){
+				logger.error("InputStream is null");
+			}else {
+				XSDHelper.INSTANCE.define(is, null);
+
+				is.close();
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			logger.error("SIMPL-XSD-File not found", e1);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			logger.error("InputStream Exception", e);
+			logger.error("Exception while opening the stream", e);
 		}
 	}
 }
