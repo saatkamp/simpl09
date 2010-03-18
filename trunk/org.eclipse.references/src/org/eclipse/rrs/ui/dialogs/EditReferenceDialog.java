05f8d254fdcf71965ea6caa560a6223b26a68720
hahnml@t-online.de <hahnml@t-online.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2010-03-18 14:26:18 +0000

    Einfügen eines Menüs und einer Toolbar und Erstellung entsprechender Commands und Handler für das Erstellen und Löschen von Referenzen im RRS View.

---
+++ trunk/org.eclipse.references/src/org/eclipse/rrs/ui/dialogs/EditReferenceDialog.java
@@ -0,0 +1,13 @@
+package org.eclipse.rrs.ui.dialogs;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class EditReferenceDialog extends TitleAreaDialog {
+
+	public EditReferenceDialog(Shell parentShell) {
+		super(parentShell);
+		// TODO Auto-generated constructor stub
+	}
+
+}
