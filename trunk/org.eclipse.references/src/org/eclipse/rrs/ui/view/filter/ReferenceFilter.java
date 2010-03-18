7b9c4f426d9b4f4a68cacbdf8fbff7c9f1aaa38c
hahnml@t-online.de <hahnml@t-online.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2010-03-18 16:44:53 +0000

    Fertigstellung aller grundlegenden Funktionen des RRS Eclipse Plug-Ins.

---
+++ trunk/org.eclipse.references/src/org/eclipse/rrs/ui/view/filter/ReferenceFilter.java
@@ -0,0 +1,37 @@
+package org.eclipse.rrs.ui.view.filter;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.rrs.model.reference.EPR;
+
+public class ReferenceFilter extends ViewerFilter {
+
+	private String searchString;
+
+	public void setSearchText(String s) {
+		// Search must be a substring of the existing value
+		this.searchString = ".*" + s + ".*";
+	}
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		if (searchString == null || searchString.length() == 0) {
+			return true;
+		}
+		EPR epr = (EPR) element;
+		if (epr.getReferenceParameters().getReferenceName().matches(searchString)) {
+			return true;
+		}
+		if (epr.getAddress().getUri().matches(searchString)) {
+			return true;
+		}
+		if (epr.getReferenceProperties().getResolutionSystem().getAdapterURI().matches(searchString)) {
+			return true;
+		}
+		if (epr.getReferenceParameters().getStatement().matches(searchString)) {
+			return true;
+		}
+
+		return false;
+	}
+}
