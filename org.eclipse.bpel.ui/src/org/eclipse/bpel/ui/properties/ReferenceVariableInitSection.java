940244358310bc83edf2d9f6ba5006394e432a1a
hahnml <hahnml@t-online.de>
2010-04-09 12:59:55 +0000

    Erstellung einer neuen BPELSection zur Angabe des ReferenceType einer ReferenceVariable.

--- org.eclipse.bpel.ui/src/org/eclipse/bpel/ui/properties/ReferenceVariableInitSection.java
+++ org.eclipse.bpel.ui/src/org/eclipse/bpel/ui/properties/ReferenceVariableInitSection.java
@@ -4,12 +4,10 @@ import org.eclipse.bpel.common.ui.details.IDetailsAreaConstants;
 import org.eclipse.bpel.common.ui.details.widgets.DecoratedLabel;
 import org.eclipse.bpel.common.ui.flatui.FlatFormAttachment;
 import org.eclipse.bpel.common.ui.flatui.FlatFormData;
-import org.eclipse.bpel.model.BPELFactory;
 import org.eclipse.bpel.model.BPELPackage;
 import org.eclipse.bpel.model.ReferenceType;
 import org.eclipse.bpel.model.ReferenceVariable;
 import org.eclipse.bpel.ui.commands.SetReferenceVariableReferenceTypeCommand;
-import org.eclipse.bpel.ui.commands.SetReferenceVariableValueTypeCommand;
 import org.eclipse.bpel.ui.util.BPELUtil;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EEnumLiteral;
@@ -19,55 +17,66 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 
 /**
- * <b>Purpose:</b> Provides UI elements to initialize all other (not the valueType) elements
- * of a ReferenceVariable in the "Details" section of a ReferenceVariable. <br>
+ * <b>Purpose:</b> Provides UI elements to initialize all other (not the
+ * valueType) elements of a ReferenceVariable in the "Details" section of a
+ * ReferenceVariable. <br>
  * <b>Description:</b> <br>
- * <b>Copyright:</b>  Licensed under the Apache License, Version 2.0. http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Copyright:</b> Licensed under the Apache License, Version 2.0.
+ * http://www.apache.org/licenses/LICENSE-2.0<br>
  * <b>Company:</b> SIMPL<br>
  * 
  * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de> <br>
  * @version $Id$ <br>
  * @link http://code.google.com/p/simpl09/
- *
+ * 
  */
 public class ReferenceVariableInitSection extends BPELPropertySection {
-	
+
 	protected CCombo fReferenceTypeCombo;
-	private ReferenceVariable referenceVariable;
-	
-	private Label refTypeLabel;
-	private Text refTypeName;
-	
+
+	@Override
+	protected void basicSetInput(EObject input) {
+		super.basicSetInput(input);
+	}
+
 	protected void createReferenceTypeWidgets(Composite composite) {
 		FlatFormData data;
 
-		DecoratedLabel refTypeLabel = new DecoratedLabel(composite,SWT.LEFT);
-		fWidgetFactory.adapt(refTypeLabel);		
+		DecoratedLabel refTypeLabel = new DecoratedLabel(composite, SWT.LEFT);
+		fWidgetFactory.adapt(refTypeLabel);
 		refTypeLabel.setText("Reference type:");
-		refTypeLabel.setToolTipText("Specify which kind of reference should be used.");
-		
+		refTypeLabel
+				.setToolTipText("Specify which kind of reference should be used.");
+
 		fReferenceTypeCombo = fWidgetFactory.createCCombo(composite, SWT.NONE);
 		data = new FlatFormData();
-		data.left = new FlatFormAttachment(0, BPELUtil.calculateLabelWidth(refTypeLabel, STANDARD_LABEL_WIDTH_AVG));
-		data.right = new FlatFormAttachment(30, (-2) * IDetailsAreaConstants.HSPACE );
-		data.top = new FlatFormAttachment(0, IDetailsAreaConstants.VSPACE );
+		data.left = new FlatFormAttachment(0, BPELUtil.calculateLabelWidth(
+				refTypeLabel, STANDARD_LABEL_WIDTH_AVG));
+		data.right = new FlatFormAttachment(30, (-2)
+				* IDetailsAreaConstants.HSPACE);
+		data.top = new FlatFormAttachment(0, IDetailsAreaConstants.VSPACE);
 		fReferenceTypeCombo.setLayoutData(data);
 
 		data = new FlatFormData();
 		data.left = new FlatFormAttachment(0, 0);
-		data.right = new FlatFormAttachment(fReferenceTypeCombo, -IDetailsAreaConstants.HSPACE);
+		data.right = new FlatFormAttachment(fReferenceTypeCombo,
+				-IDetailsAreaConstants.HSPACE);
 		data.top = new FlatFormAttachment(fReferenceTypeCombo, 0, SWT.CENTER);
-		refTypeLabel.setLayoutData(data);	
-		
-		//Load the model data in the referenceType combo
+		refTypeLabel.setLayoutData(data);
+
+		// Load the model data in the referenceType combo
 		fReferenceTypeCombo.setItems(getReferenceTypes());
-		
-		fReferenceTypeCombo.addSelectionListener(new SelectionListener(){
+
+		// ReferenceVariable variable = (ReferenceVariable) getInput();
+		// fReferenceTypeCombo.select(fReferenceTypeCombo
+		// .indexOf(variable.getReferenceType()
+		// .getName()));
+
+		fReferenceTypeCombo.addSelectionListener(new SelectionListener() {
 
 			public void widgetDefaultSelected(SelectionEvent e) {
 				// TODO Auto-generated method stub
@@ -76,31 +85,42 @@ public class ReferenceVariableInitSection extends BPELPropertySection {
 
 			public void widgetSelected(SelectionEvent e) {
 				// TODO Auto-generated method stub
-				String selection = fReferenceTypeCombo.getItem(fReferenceTypeCombo.getSelectionIndex());
-				
-				ReferenceVariable variable = (ReferenceVariable)getInput();
-		    	Command cmd = new SetReferenceVariableReferenceTypeCommand(variable, selection.equals(ReferenceType.ON_INSTANTIATION.getName()) ? ReferenceType.ON_INSTANTIATION : ReferenceType.FRESH);
+				String selection = fReferenceTypeCombo
+						.getItem(fReferenceTypeCombo.getSelectionIndex());
+
+				ReferenceVariable variable = (ReferenceVariable) getInput();
+
+				Command cmd = new SetReferenceVariableReferenceTypeCommand(
+						variable,
+						selection.equals(ReferenceType.ON_INSTANTIATION
+								.getName()) ? ReferenceType.ON_INSTANTIATION
+								: ReferenceType.FRESH);
 				getCommandFramework().execute(wrapInShowContextCommand(cmd));
-			}});
+			}
+		});
 	}
-	
+
 	@Override
 	protected void createClient(Composite parent) {
+		//TODO: Laden des im Modell gespeicherten ReferenceType funktioniert noch nicht.
+		//Das Speichern ist bereits umgesetzt und funktioniert.
+		
 		Composite composite = createFlatFormComposite(parent);
 		createReferenceTypeWidgets(composite);
 	}
-	
-	private String[] getReferenceTypes(){
-		EList<EEnumLiteral> literals = BPELPackage.eINSTANCE.getReferenceType().getELiterals();
-		
+
+	private String[] getReferenceTypes() {
+		EList<EEnumLiteral> literals = BPELPackage.eINSTANCE.getReferenceType()
+				.getELiterals();
+
 		String[] types = new String[literals.size()];
-		
+
 		int index = 0;
-		for (EEnumLiteral literal : literals){
+		for (EEnumLiteral literal : literals) {
 			types[index] = literal.getName();
 			index++;
 		}
-		
+
 		return types;
 	}
 }
