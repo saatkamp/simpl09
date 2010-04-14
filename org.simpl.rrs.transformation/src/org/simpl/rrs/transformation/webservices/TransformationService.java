4e5e4a0669f4283d970732a32f9e11fae8f46086
hahnml <hahnml@t-online.de>
2010-04-14 11:41:04 +0000

    Fertigstellung des Transformation Service für ODE (WebService und Client erstellt).

---
+++ org.simpl.rrs.transformation/src/org/simpl/rrs/transformation/webservices/TransformationService.java
@@ -0,0 +1,32 @@
+package org.simpl.rrs.transformation.webservices;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+import org.simpl.rrs.transformation.Transformer;
+
+/**
+ * <b>Purpose:</b> <br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b> Licensed under the Apache License, Version 2.0.
+ * http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de> <br>
+ * @version $Id$ <br>
+ * @link http://code.google.com/p/simpl09/
+ * 
+ */
+@WebService(name = "TransformationService", targetNamespace = "")
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public class TransformationService {
+	@WebMethod(action = "transform")
+	public String transform(
+			@WebParam(name = "bpelFileContent", targetNamespace = "") String bpelFileContent) {
+		String response = Transformer.getTransformer().transform(bpelFileContent);
+
+		return response;
+	}
+}
