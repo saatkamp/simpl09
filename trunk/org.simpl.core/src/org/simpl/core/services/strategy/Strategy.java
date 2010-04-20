3cadb8c08adf34f0113c9af235cb480ae6136103
michael.schneidt@arcor.de <michael.schneidt@arcor.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2010-04-20 18:19:11 +0000

    StrategyService Implementierung

---
+++ trunk/org.simpl.core/src/org/simpl/core/services/strategy/Strategy.java
@@ -0,0 +1,16 @@
+package org.simpl.core.services.strategy;
+
+/**
+ * <b>Purpose:</b> Provides the different types of strategies for the strategy
+ * service.<br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b> <br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Schneidt <michael.schneidt@arcor.de><br>
+ * @version $Id$<br>
+ * @link http://code.google.com/p/simpl09/
+ */
+public enum Strategy {
+  FIRST_FIND
+}
--- trunk/org.simpl.core/src/org/simpl/core/services/strategy/StrategyService.java
+++ trunk/org.simpl.core/src/org/simpl/core/services/strategy/StrategyService.java
@@ -1,16 +1,18 @@
 package org.simpl.core.services.strategy;
+
+import org.simpl.core.services.datasource.DataSourceService;
+
 /**
- * <b>Purpose:</b>
- * <br>
- * <b>Description:</b>
- * <br>
- * <b>Copyright:</b>     <br>
- * <b>Company:</b>       SIMPL<br>
- *
- * @author      Michael Schneidt <michael.schneidt@arcor.de><br>
- * @version     $Id$<br>
+ * <b>Purpose:</b> Defines the functions of the strategy service.<br>
+ * <b>Description:</b><br>
+ * <b>Copyright:</b> <br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Schneidt <michael.schneidt@arcor.de><br>
+ * @version $Id$<br>
  * @link http://code.google.com/p/simpl09/
  */
 public interface StrategyService {
-
+  public DataSourceService findDataSourceService(String wsPolicy,
+      Strategy strategy);
 }
