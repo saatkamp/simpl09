29b00a1a890b8a184f1402de0e42580fc464e396
michael.schneidt <michael.schneidt@arcor.de>
2010-04-22 14:13:17 +0000

    - Subtype von DataFormat entfernt - Kopf-Kommentare ergänzt - init() Methode von DataFormatPlugin entfernt

--- org.simpl.core/src/org/simpl/core/plugins/dataformat/DataFormatPlugin.java
+++ org.simpl.core/src/org/simpl/core/plugins/dataformat/DataFormatPlugin.java
@@ -2,8 +2,6 @@ package org.simpl.core.plugins.dataformat;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.simpl.core.services.dataformat.DataFormatService;
 
@@ -12,127 +10,61 @@ import commonj.sdo.helper.DataFactory;
 import commonj.sdo.helper.XSDHelper;
 
 /**
- * <b>Purpose:</b> ... <br>
- * <b>Description:</b> service data object (SDO) <br>
+ * <b>Purpose:</b>This abstract class is used to create data format plugins for
+ * the SIMPL Core, that are used by the data source service to support different
+ * data formats of different data sources. A data format is used by a data
+ * source service to map outgoing data to and incoming data from a Service Data
+ * Object (SDO) using Apache Tuscany.<br>
+ * <b>Description:</b>The type or rather name of data format is set with
+ * {@link #setType(String)}. The structure of the SDO is defined by a XML-schema
+ * file, that is set with {@link #setSchemaFile(String)}, and supposed to be
+ * deployed in a .jar file along with the extending plugin class. In order to
+ * create an empty SDO from the schema that can be filled with data, a root
+ * schema type has to be set with #setSchemaType(String). All settings have to
+ * be done in the plugin's constructor, the created SDO is returned by
+ * {@link #getDataObject()}.<br>
  * <b>Copyright:</b> <br>
- * <b>Company:</b> SIMPL<br>
+ * <b>Company:</b>SIMPL<br>
  * 
  * @author schneimi
- * @version $Id: DataFormatPlugin.java 1006 2010-03-24 17:52:54Z michael.schneidt@arcor.de
- *          $<br>
+ * @version $Id: DataFormatPlugin.java 1006 2010-03-24 17:52:54Z
+ *          michael.schneidt@arcor.de $<br>
  * @link http://code.google.com/p/simpl09/
  */
 public abstract class DataFormatPlugin<T> implements DataFormatService<T> {
   /**
-   * Name of the data format schema file.
-   */
-  private String dataFormatSchemaFile = "DefaultDataFormat.xsd";
-
-  /**
    * Type of the supporting data format (CSV, XML, ...).
    */
-  private String dataFormatType = "DefaultFormat";
+  private String type = "DefaultFormat";
 
   /**
-   * The data format schema type defined in the data format schema file that is used to
-   * create the data object.
+   * Name of the data format schema file.
    */
-  private String dataFormatSchemaType = "dataObject";
+  private String schemaFile = "DefaultDataFormat.xsd";
 
   /**
-   * Subtypes of the supporting data format (CSVWithHeadline, XMLForSimulation, ..). TODO:
-   * bessere Beispiele finden
+   * The data format schema type defined in the data format schema file that is
+   * used to create the data object.
    */
-  private List<String> dataFormatSubtypes = new ArrayList<String>();
+  private String schemaType = "tDataObject";
 
   /**
-   * Data object created from the data format schema file for holding the data.
+   * @return Empty SDO created from the data format schema.
    */
-  private DataObject dataObject = null;
-
-  /**
-   * Initializes the data format in creating the data object from the schema file.
-   */
-  public void init() {
-    this.dataObject = createDataObject();
-  }
-
-  /**
-   * @return Empty data object following the data format schema
-   */
-  public DataObject getDataObjectFromSchema() {
-    return this.dataObject;
-  }
-
-  /**
-   * Sets the supporting data format type.
-   * 
-   * @param dfType
-   */
-  public void setType(String dfType) {
-    this.dataFormatType = dfType;
-  }
-
-  /**
-   * Sets the element type that is defined in the data format schema file.
-   * 
-   * @param type
-   */
-  public void setSchemaType(String dfSchemaType) {
-    this.dataFormatSchemaType = dfSchemaType;
-  }
-
-  /**
-   * Sets the name/location of the data format schema file.
-   * 
-   * @param dfSchemaFile
-   */
-  public void setSchemaFile(String dfSchemaFile) {
-    this.dataFormatSchemaFile = dfSchemaFile;
-  }
-
-  /**
-   * @return List of supporting data format subtypes.
-   */
-  public List<String> getSubtypes() {
-    return this.dataFormatSubtypes;
-  }
-
-  /**
-   * Adds a supporting data format subtype.
-   * 
-   * @param dfSubtype
-   */
-  public void addSubtype(String dfSubtype) {
-    if (!this.dataFormatSubtypes.contains(dfSubtype)) {
-      this.dataFormatSubtypes.add(dfSubtype);
-    }
-  }
-
-  /**
-   * @return The supporting data format type.
-   */
-  public String getType() {
-    return this.dataFormatType;
-  }
-
-  /**
-   * @return Emty data object created from the data format schema.
-   */
-  private DataObject createDataObject() {
+  public DataObject getSDO() {
     DataObject dataObject = null;
     InputStream inputStream = null;
 
     // load the schema file
-    if (this.dataFormatSchemaFile.equals("DefaultDataFormat.xsd")) {
+    if (this.schemaFile.equals("DefaultDataFormat.xsd")) {
       inputStream = getClass().getResourceAsStream(
-          "/org/simpl/core/plugins/dataformat/" + this.dataFormatSchemaFile);
+          "/org/simpl/core/plugins/dataformat/" + this.schemaFile);
     } else {
-      inputStream = getClass().getResourceAsStream(this.dataFormatSchemaFile);
+      inputStream = getClass().getResourceAsStream(this.schemaFile);
     }
 
     if (inputStream == null) {
-      System.out.println("The file '" + this.dataFormatSchemaFile
+      System.out.println("The file '" + this.schemaFile
           + "' could not be found.");
     }
 
@@ -145,8 +77,42 @@ public abstract class DataFormatPlugin<T> implements DataFormatService<T> {
     }
 
     dataObject = DataFactory.INSTANCE.create(
-        "http://org.simpl.core/plugins/dataformat/DataFormat", this.dataFormatSchemaType);
+        "http://org.simpl.core/plugins/dataformat/DataFormat", this.schemaType);
 
     return dataObject;
+  }
+
+  /**
+   * Sets the supporting data format type.
+   * 
+   * @param dfType
+   */
+  public void setType(String dfType) {
+    this.type = dfType;
+  }
+
+  /**
+   * Sets the element type that is defined in the data format schema file.
+   * 
+   * @param type
+   */
+  public void setSchemaType(String dfSchemaType) {
+    this.schemaType = dfSchemaType;
+  }
+
+  /**
+   * Sets the name and location of the data format schema file.
+   * 
+   * @param dfSchemaFile
+   */
+  public void setSchemaFile(String dfSchemaFile) {
+    this.schemaFile = dfSchemaFile;
+  }
+
+  /**
+   * @return The supporting data format type.
+   */
+  public String getType() {
+    return this.type;
   }
 }
