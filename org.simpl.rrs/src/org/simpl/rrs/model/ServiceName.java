8e6ffac394c4b579ac4f0c9373749b6b3848b1ae
hahnml <hahnml@t-online.de>
2010-05-03 19:28:17 +0000

    Umstrukturierung der EPR-Klasse und Implementierung der getEPR()-Methode des MetaDataService.

---
+++ org.simpl.rrs/src/org/simpl/rrs/model/ServiceName.java
@@ -0,0 +1,37 @@
+package org.simpl.rrs.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * <b>Purpose:</b> <br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b>  Licensed under the Apache License, Version 2.0. http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de> <br>
+ * @version $Id$ <br>
+ * @link http://code.google.com/p/simpl09/
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ServiceName", propOrder = { "serviceName", "portName"})
+public class ServiceName {
+	
+	private String serviceName = null;
+	private String portName = null;
+	
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	public String getPortName() {
+		return portName;
+	}
+	public void setPortName(String portName) {
+		this.portName = portName;
+	}
+}
