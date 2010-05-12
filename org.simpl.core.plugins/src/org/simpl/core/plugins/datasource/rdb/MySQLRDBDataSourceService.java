05ee2c3c31d1a8f7620aceb70c8ea154de2a1803
michael.schneidt <michael.schneidt@arcor.de>
2010-05-12 12:31:47 +0000

    - db disconnect in RDBDataFormat workaround

--- org.simpl.core.plugins/src/org/simpl/core/plugins/datasource/rdb/MySQLRDBDataSourceService.java
+++ org.simpl.core.plugins/src/org/simpl/core/plugins/datasource/rdb/MySQLRDBDataSourceService.java
@@ -94,10 +94,6 @@ public class MySQLRDBDataSourceService extends
 
       rdbResult.setDbMetaData(connection.getMetaData());
       rdbResult.setResultSet(resultSet);
-
-      connStatement.close();
-      connection.commit();
-      closeConnection(connection);
     } catch (SQLException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
