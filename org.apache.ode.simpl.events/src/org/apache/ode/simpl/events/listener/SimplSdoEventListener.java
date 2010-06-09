8887d7ca46944ed15b3041aa0385ab3f6e0a90e7
w.huettig <w.huettig@yahoo.de>
2010-06-09 09:28:10 +0000

    Auditing

--- org.apache.ode.simpl.events/src/org/apache/ode/simpl/events/listener/SimplSdoEventListener.java
+++ org.apache.ode.simpl.events/src/org/apache/ode/simpl/events/listener/SimplSdoEventListener.java
@@ -1,7 +1,5 @@
 package org.apache.ode.simpl.events.listener;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Properties;
@@ -9,6 +7,7 @@ import java.util.Properties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.ode.bpel.evt.ActivityEvent;
+import org.apache.ode.bpel.evt.ActivityFailureEvent;
 import org.apache.ode.bpel.evt.BpelEvent;
 import org.apache.ode.bpel.evt.CorrelationEvent;
 import org.apache.ode.bpel.evt.CorrelationMatchEvent;
@@ -31,6 +30,9 @@ import org.apache.ode.bpel.evt.VariableEvent;
 import org.apache.ode.bpel.iapi.BpelEventListener;
 import org.apache.ode.bpel.pmapi.TEventInfo;
 
+
+//import commonj.sdo.DataObject;
+
 /**
  * <b>Purpose:</b> <br>
  * <b>Description:</b> This implementation is based on the JmsBpelEventListener
@@ -50,13 +52,15 @@ public class SimplSdoEventListener implements BpelEventListener {
 
 	public static final String TOPIC_NAME_KEY = "jel.topicname";
 	public static final String MQ_URL_KEY = "jel.mqurl";
-	private String topicName;
-	private String url;
+	
+	EventWriter writer = new EventWriter();
+//	private String topicName;
+//	private String url;
 
 	boolean initialized = false;
 	protected Calendar _calendar = Calendar.getInstance();
 
-	private int counter = 0;
+//	private int counter = 0;
 
 	public void onEvent(BpelEvent bpelEvent) {
 		if (!initialized)
@@ -71,18 +75,24 @@ public class SimplSdoEventListener implements BpelEventListener {
 
 	// Test
 	protected void saveEventInfo(BpelEvent evt) {
-		TEventInfo ei = TEventInfo.Factory.newInstance();
-		fillEventInfo(ei, evt);
-		try {
-			File file = new File("C:/test/eventFile"+counter +".txt");
-			ei.save(file);
-			counter++;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String xml = ei.toString();
-		logger.debug(xml);
+		 TEventInfo ei = TEventInfo.Factory.newInstance();
+		 fillEventInfo(ei, evt);
+		// try {
+		// File file = new File("C:/test/eventFile"+counter +".txt");
+		// ei.save(file);
+		// counter++;
+		//			
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// String xml = ei.toString();
+		// logger.debug(xml);
+
+//		DataObject rdbDataObject = this.getSDO();
+		
+		
+		writer.write(ei);
 	}
 
 	public void shutdown() {
@@ -99,7 +109,7 @@ public class SimplSdoEventListener implements BpelEventListener {
 		// e.printStackTrace();
 		// }
 		// factory = null;
-		
+
 		initialized = false;
 		logger.info("SIMPL BPEL event has been shutdown.");
 	}
@@ -125,7 +135,8 @@ public class SimplSdoEventListener implements BpelEventListener {
 		// initialized = true;
 		// } catch (JMSException e) {
 		// logger.error("Initialization failed.", e);
-		// }
+		
+		writer = new EventWriter();
 		initialized = true;
 
 		logger.info("SIMPL BPEL event has been started.");
@@ -151,7 +162,14 @@ public class SimplSdoEventListener implements BpelEventListener {
 			info.setActivityType(((ActivityEvent) event).getActivityType());
 			info.setActivityDefinitionId(((ActivityEvent) event)
 					.getActivityDeclarationId());
+
 		}
+
+		if (event instanceof ActivityFailureEvent) {
+			info.setActivityFailureReason(((ActivityFailureEvent) event)
+					.getFailureReason());
+		}
+
 		if (event instanceof CorrelationEvent) {
 			info.setPortType(((CorrelationEvent) event).getPortType());
 			info.setOperation(((CorrelationEvent) event).getOperation());
@@ -255,4 +273,29 @@ public class SimplSdoEventListener implements BpelEventListener {
 		c.setTime(dtime);
 		return c;
 	}
+
+//	public DataObject getSDO() {
+//		DataObject dataObject = null;
+//		DataSource dataSource = new DataSource();
+//		
+//		dataObject = DataFormatProvider.getInstance("RDB").getSDO();
+//
+//		// put data into the SDO
+//		
+//		Printer.printDataObjectXML(dataObject);
+//		
+//		dataSource.setType("Database");
+//		dataSource.setSubType("EmbeddedDerby");
+//		dataSource.setAddress("C:\\test\\auditing");
+//		dataSource.setDataFormat("RDB");
+//
+//		try {
+//			SIMPLCore.getInstance().dataSourceService().writeData(dataSource, dataObject, "auditing_table");
+//		} catch (ConnectionException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		return dataObject;
+//	}
 }
