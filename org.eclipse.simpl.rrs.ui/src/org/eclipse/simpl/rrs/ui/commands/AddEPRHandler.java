837a2a38ea890113786995abd9a2b605363e896f
hahnml <hahnml@t-online.de>
2010-07-04 09:48:00 +0000

    RRS-Plug-In: RRSView liefert Fehlermeldungen falls das RRS nicht erreichbar ist. SIMPL Core Plug-In: ReloadPlugInData gibt Erfolgs- bzw. Fehlermeldung.

--- org.eclipse.simpl.rrs.ui/src/org/eclipse/simpl/rrs/ui/commands/AddEPRHandler.java
+++ org.eclipse.simpl.rrs.ui/src/org/eclipse/simpl/rrs/ui/commands/AddEPRHandler.java
@@ -1,15 +1,15 @@
 package org.eclipse.simpl.rrs.ui.commands;
 
-import java.util.List;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.simpl.rrs.ui.client.EPR;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.simpl.rrs.ui.client.ModelProvider;
+import org.eclipse.simpl.rrs.ui.client.RRSClient;
 import org.eclipse.simpl.rrs.ui.dialogs.AddReferenceDialog;
 import org.eclipse.simpl.rrs.ui.view.ReferenceManagementView;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -18,29 +18,45 @@ public class AddEPRHandler extends AbstractHandler implements IHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
-		AddReferenceDialog dialog = new AddReferenceDialog(window.getShell());
-		dialog.open();
-		if (dialog.getReference() != null) {
-			ModelProvider.getInstance().getReferences().add(dialog.getReference());
-			ModelProvider.getInstance().insertReference(dialog.getReference());
-			
-			// Updating the display in the view
-			IWorkbenchPage page = window.getActivePage();
-			ReferenceManagementView view = (ReferenceManagementView) page.findView(ReferenceManagementView.ID);
-			view.getViewer().refresh();
+		if (RRSClient.getClient().isRRSAvailable()) {
+			IWorkbenchWindow window = HandlerUtil
+					.getActiveWorkbenchWindow(event);
+			AddReferenceDialog dialog = new AddReferenceDialog(window
+					.getShell());
+			dialog.open();
+			if (dialog.getReference() != null) {
+				ModelProvider.getInstance().getReferences().add(
+						dialog.getReference());
+				ModelProvider.getInstance().insertReference(
+						dialog.getReference());
+
+				// Updating the display in the view
+				IWorkbenchPage page = window.getActivePage();
+				ReferenceManagementView view = (ReferenceManagementView) page
+						.findView(ReferenceManagementView.ID);
+				view.getViewer().refresh();
+			}
+
+			// for (EPR reference : references){
+			// System.out.println("Name: " +
+			// reference.getReferenceParameters().getReferenceName());
+			// System.out.println("Address: " +
+			// reference.getAddress().getUri());
+			// System.out.println("Adapter: " +
+			// reference.getReferenceProperties().getResolutionSystem().getAdapterURI());
+			// System.out.println("Statement: " +
+			// reference.getReferenceParameters().getStatement());
+			// System.out.println("--------------------------------------------");
+			// }
+		} else {
+			MessageDialog
+			.openError(
+					Display.getCurrent().getActiveShell(),
+					"RRS Connection Exception",
+					"The RRS isn't available. Please check that the RRS works properly and that the specified addresses in the SIMPL preferences are correct.");
 		}
-		
-//		for (EPR reference : references){
-//			System.out.println("Name: " + reference.getReferenceParameters().getReferenceName());
-//			System.out.println("Address: " + reference.getAddress().getUri());
-//			System.out.println("Adapter: " + reference.getReferenceProperties().getResolutionSystem().getAdapterURI());
-//			System.out.println("Statement: " + reference.getReferenceParameters().getStatement());
-//			System.out.println("--------------------------------------------");
-//		}
-		
+
 		return null;
 	}
-
 
 }
--- org.eclipse.simpl.rrs.ui/src/org/eclipse/simpl/rrs/ui/commands/RefreshHandler.java
+++ org.eclipse.simpl.rrs.ui/src/org/eclipse/simpl/rrs/ui/commands/RefreshHandler.java
@@ -3,31 +3,43 @@ package org.eclipse.simpl.rrs.ui.commands;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.simpl.rrs.ui.client.ModelProvider;
+import org.eclipse.simpl.rrs.ui.client.RRSClient;
 import org.eclipse.simpl.rrs.ui.view.ReferenceManagementView;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * <b>Purpose:</b> <br>
  * <b>Description:</b> <br>
- * <b>Copyright:</b>  Licensed under the Apache License, Version 2.0. http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Copyright:</b> Licensed under the Apache License, Version 2.0.
+ * http://www.apache.org/licenses/LICENSE-2.0<br>
  * <b>Company:</b> SIMPL<br>
  * 
  * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de> <br>
  * @version $Id$ <br>
  * @link http://code.google.com/p/simpl09/
- *
+ * 
  */
 public class RefreshHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ModelProvider.getInstance().refresh();
-		IWorkbenchPart part = HandlerUtil.getActivePart(event);
-		if (part instanceof ReferenceManagementView){
-			ReferenceManagementView view = (ReferenceManagementView) part;
-			view.getViewer().refresh();
+		if (RRSClient.getClient().isRRSAvailable()) {
+			ModelProvider.getInstance().refresh();
+			IWorkbenchPart part = HandlerUtil.getActivePart(event);
+			if (part instanceof ReferenceManagementView) {
+				ReferenceManagementView view = (ReferenceManagementView) part;
+				view.getViewer().refresh();
+			}
+		}else {
+			MessageDialog
+			.openError(
+					Display.getCurrent().getActiveShell(),
+					"RRS Connection Exception",
+					"The RRS isn't available. Please check that the RRS works properly and that the specified addresses in the SIMPL preferences are correct.");
 		}
 		return null;
 	}
