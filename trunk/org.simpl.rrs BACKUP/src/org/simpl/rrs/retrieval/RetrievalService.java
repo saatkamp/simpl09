43bb97857b3845e5662f9e3b18f7f05602912a28
hahnml@t-online.de <hahnml@t-online.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2010-07-16 13:37:29 +0000

    Veraltete Projekte/Dateien aus \trunk gelöscht.

--- trunk/org.simpl.rrs BACKUP/src/org/simpl/rrs/retrieval/RetrievalService.java
+++
@@ -1,9 +0,0 @@
-package org.simpl.rrs.retrieval;
-
-import org.simpl.rrs.webservices.client.EPR;
-
-public interface RetrievalService {
-
-	public Object get(EPR epr);
-	
-}
--- trunk/org.simpl.rrs BACKUP/src/org/simpl/rrs/webservices/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservices.rrs.simpl.org/")
-package org.simpl.rrs.webservices.client;
