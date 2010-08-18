2726d32cecfcaba2f37f5a1d0232e978dca0e59e
michael.schneidt@arcor.de <michael.schneidt@arcor.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2010-08-18 15:43:55 +0000

    org.eclipse.bpel.simpl.ui hiwi branch

---
+++ branches/hiwi/org.eclipse.bpel.simpl.ui/src/org/eclipse/bpel/simpl/ui/properties/util/PropertySectionUtils.java
@@ -0,0 +1,324 @@
+package org.eclipse.bpel.simpl.ui.properties.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.bpel.model.Process;
+import org.eclipse.bpel.ui.IBPELUIConstants;
+import org.eclipse.bpel.ui.util.BPELUtil;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.simpl.communication.SIMPLCommunication;
+import org.eclipse.simpl.communication.client.Authentication;
+import org.eclipse.simpl.communication.client.DataSource;
+import org.eclipse.simpl.uddi.model.ModelProvider;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+
+/**
+ * <b>Purpose:</b> <br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b> Licensed under the Apache License, Version 2.0.
+ * http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de> <br>
+ * @version $Id$ <br>
+ * @link http://code.google.com/p/simpl09/
+ * 
+ */
+public class PropertySectionUtils {
+
+	private static final String EL_PROCESS = "process";
+	private static final String EL_DATASOURCE = "datasources";
+
+	private static final String AT_NAME = "name";
+	private static final String AT_DATA_SOURCE_NAME = "dataSourceName";
+	private static final String AT_DATA_SOURCE_ADDRESS = "address";
+	private static final String AT_DATA_SOURCE_TYPE = "type";
+	private static final String AT_DATA_SOURCE_SUBTYPE = "subtype";
+	private static final String AT_DATA_SOURCE_LANG = "language";
+	private static final String AT_DATA_SOURCE_USERNAME = "userName";
+	private static final String AT_DATA_SOURCE_PASSWORD = "password";
+	private static String AT_DATA_FORMAT = "format";
+
+	/**
+	 * Die BPEL Datei des Prozesses
+	 */
+	private static IFile bpelFile = null;
+
+	/**
+	 * Der Pfad zur BPEL Datei des Prozesses
+	 */
+	private static IPath bpelPath = null;
+
+	/**
+	 * Pfad zum Projektordner = Pfad zur BPEL Datei ohne Dateiendung
+	 */
+	private static IPath projectPath = null;
+
+	/**
+	 * Absoluter Workspace-Pfad. Pfad in Format "OSString" umwandeln, so dass
+	 * man mit java.io arbeiten kann.
+	 */
+	private static String absolutWorkspacePath = "";
+
+	private final static Namespace DD_NAMESPACE = Namespace
+			.getNamespace("http://www.apache.org/ode/schemas/dd/2007/03");
+
+	private static final String UDDI_PREFIX = "uddi";
+
+	private static final String DD_PREFIX = "dd";
+
+	private static DataSource findDeploymentDescriptorDatasourceByName(
+			Process process, String dsName) {
+		DataSource datasource = null;
+
+		bpelFile = BPELUtil.getBPELFile(process);
+
+		bpelPath = bpelFile.getFullPath();
+
+		projectPath = bpelPath.removeFileExtension().removeLastSegments(1);
+
+		absolutWorkspacePath = ResourcesPlugin.getWorkspace().getRoot()
+				.getLocation().toOSString();
+
+		File deploy = new File(absolutWorkspacePath + projectPath.toOSString()
+				+ System.getProperty("file.separator") + "deploy.xml");
+
+		if (deploy.exists()) {
+			SAXBuilder builder = new SAXBuilder();
+			Document doc;
+			try {
+				InputStream inputStream = new FileInputStream(deploy);
+
+				doc = builder.build(inputStream);
+
+				Element root = doc.getRootElement();
+
+				List processes = root.getChildren(EL_PROCESS, DD_NAMESPACE);
+
+				for (Object processObj : processes) {
+					Element processElement = (Element) processObj;
+					if (processElement.getAttributeValue(AT_NAME).contains(
+							process.getName())) {
+						List datasourceElements = processElement.getChildren(
+								EL_DATASOURCE, DD_NAMESPACE);
+						for (Object data : datasourceElements) {
+							String name = ((Element) data)
+									.getAttributeValue(AT_DATA_SOURCE_NAME);
+							if (name.equals(dsName)) {
+								datasource = new DataSource();
+								datasource.setName(name);
+								datasource
+										.setAddress(((Element) data)
+												.getAttributeValue(AT_DATA_SOURCE_ADDRESS));
+								datasource
+										.setType(((Element) data)
+												.getAttributeValue(AT_DATA_SOURCE_TYPE));
+								datasource
+										.setSubType(((Element) data)
+												.getAttributeValue(AT_DATA_SOURCE_SUBTYPE));
+								datasource
+										.setLanguage(((Element) data)
+												.getAttributeValue(AT_DATA_SOURCE_LANG));
+								datasource.setDataFormat(((Element) data)
+										.getAttributeValue(AT_DATA_FORMAT));
+								Authentication authent = new Authentication();
+								authent
+										.setUser(((Element) data)
+												.getAttributeValue(AT_DATA_SOURCE_USERNAME));
+								authent
+										.setPassword(((Element) data)
+												.getAttributeValue(AT_DATA_SOURCE_PASSWORD));
+								datasource.setAuthentication(authent);
+							}
+						}
+					}
+				}
+
+			} catch (JDOMException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return datasource;
+	}
+
+	private static List<String> getDeploymentDescriptorDatasourceNames(
+			Process process) {
+		List<String> datasources = new ArrayList<String>();
+
+		bpelFile = BPELUtil.getBPELFile(process);
+
+		bpelPath = bpelFile.getFullPath();
+
+		projectPath = bpelPath.removeFileExtension().removeLastSegments(1);
+
+		absolutWorkspacePath = ResourcesPlugin.getWorkspace().getRoot()
+				.getLocation().toOSString();
+
+		File deploy = new File(absolutWorkspacePath + projectPath.toOSString()
+				+ System.getProperty("file.separator") + "deploy.xml");
+
+		if (deploy.exists()) {
+			SAXBuilder builder = new SAXBuilder();
+			Document doc;
+			try {
+				InputStream inputStream = new FileInputStream(deploy);
+
+				doc = builder.build(inputStream);
+
+				Element root = doc.getRootElement();
+
+				List processes = root.getChildren(EL_PROCESS, DD_NAMESPACE);
+
+				for (Object processObj : processes) {
+					Element processElement = (Element) processObj;
+					if (processElement.getAttributeValue(AT_NAME).contains(
+							process.getName())) {
+						List datasourceElements = processElement.getChildren(
+								EL_DATASOURCE, DD_NAMESPACE);
+						for (Object data : datasourceElements) {
+							String name = ((Element) data)
+									.getAttributeValue(AT_DATA_SOURCE_NAME);
+							datasources.add(DD_PREFIX + ":" + name);
+						}
+					}
+				}
+
+			} catch (JDOMException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return datasources;
+	}
+
+	private static List<String> getUDDIDatasourceNames() {
+		List<String> dataSourceNames = new ArrayList<String>();
+
+		List<DataSource> dataSources = ModelProvider.getInstance()
+				.getDataSources();
+
+		for (DataSource dat : dataSources) {
+			dataSourceNames.add(UDDI_PREFIX + ":" + dat.getName());
+		}
+
+		return dataSourceNames;
+	}
+
+	public static String[] getAllDataSourceNames(Process process) {
+		List<String> datasources = new ArrayList<String>();
+
+		datasources.addAll(getDeploymentDescriptorDatasourceNames(process));
+		datasources.addAll(getUDDIDatasourceNames());
+
+		return datasources.toArray(new String[0]);
+	}
+
+	public static DataSource findDataSourceByName(Process process,
+			String nameWithPrefix) {
+		DataSource data = null;
+
+		String[] name = nameWithPrefix.split(":");
+		if (name[0].equals(DD_PREFIX)) {
+			data = findDeploymentDescriptorDatasourceByName(process, name[1]);
+		} else {
+			if (name[0].equals(UDDI_PREFIX)) {
+				data = ModelProvider.getInstance()
+						.findDataSourceByName(name[1]);
+			}
+		}
+		return data;
+	}
+
+	public static void downloadSchema(DataSource dataSource, Process process) {
+
+		if (SIMPLCommunication.getConnection().isSIMPLCoreAvailable()
+				&& dataSource != null) {
+			bpelFile = BPELUtil.getBPELFile(process);
+
+			bpelPath = bpelFile.getFullPath();
+
+			projectPath = bpelPath.removeFileExtension().removeLastSegments(1);
+
+			absolutWorkspacePath = ResourcesPlugin.getWorkspace().getRoot()
+					.getLocation().toOSString();
+
+			if (dataSource.getDataFormat() != null) {
+
+				IPath xsdPath = projectPath.append(dataSource.getDataFormat())
+						.addFileExtension(IBPELUIConstants.EXTENSION_XSD);
+
+				File xsdFileSIMPL = new File(absolutWorkspacePath
+						+ xsdPath.toOSString());
+
+				if (!xsdFileSIMPL.exists()) {
+					try {
+
+						// TODO: Im Moment gibt es ja nur ein Datenformat
+						// pro
+						// Datenquelle, trotzdem muss die Implementierung
+						// so angepasst werden, dass das Schema über den
+						// Datenformat-Namen und nicht über ein
+						// DataSource-Objekt ausgelesen werden kann
+						// (=>Eindeutigkeit)
+						String stream = null;
+						try {
+							stream = SIMPLCommunication.getConnection()
+									.getDataFormatSchema(dataSource);
+						} catch (Exception e) {
+						}
+						if (stream != null && xsdFileSIMPL.createNewFile()) {
+
+							OutputStreamWriter out = new OutputStreamWriter(
+									new FileOutputStream(xsdFileSIMPL
+											.getAbsolutePath()), "UTF-8");
+
+							out.write(stream.toCharArray());
+							out.close();
+
+						}
+					} catch (UnsupportedEncodingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+			}
+		}
+	}
+}
---
+++ branches/hiwi/org.eclipse.bpel.simpl.ui/src/org/eclipse/bpel/simpl/ui/widgets/FileSysListPopUp.java
@@ -0,0 +1,490 @@
+/**
+ * <b>Purpose:</b> <br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b>  Licensed under the Apache License, Version 2.0. http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Firas Zoabi <zoabifs@studi.informatik.uni-stuttgart.de> <br>
+ * @version $Id: TablsListPopUp.java 1443 2010-05-21 08:46:18Z ferass_z2000@yahoo.com $ <br>
+ * @link http://code.google.com/p/simpl09/
+ *
+ */
+package org.eclipse.bpel.simpl.ui.widgets;
+
+
+import java.util.ArrayList;
+
+import org.eclipse.simpl.communication.client.DataSource;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import org.eclipse.swt.widgets.Table;
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ElementsListPopUp.
+ */
+public class FileSysListPopUp{
+	
+	
+	ArrayList<FileSystemElement> listOfFSysElements;
+	/** The text to search. */
+	Text textToSearch;
+	Text infoTextBox;
+	/** The list to search. */
+	List listToSearch,listColumns;
+	String typeOfElement="";
+	/** The array of elements. */
+	ArrayList<String> arrayOfElements=new ArrayList<String>();
+	
+	ArrayList<DBTable> listOfTableObjekts;
+	/** The window is open. */
+	boolean windowIsOpen=false;
+	
+	/**
+	 * Checks if is window open.
+	 * 
+	 * @return true, if is window open
+	 */
+	public boolean isWindowOpen() {
+		return windowIsOpen;
+	}
+
+	/**
+	 * Sets the window is open.
+	 * 
+	 * @param windowIsOpen
+	 *            the new window is open
+	 */
+	public void setWindowIsOpen(boolean windowIsOpen) {
+		this.windowIsOpen = windowIsOpen;
+	}
+
+	private Shell theShell;// =new Shell();
+	
+	/**
+	 * Instantiates a new elements list pop up.
+	 * @param typeOfElement 
+	 * 
+	 * @param statementText
+	 *            the statement text
+	 */
+	public FileSysListPopUp(String typeOfElement, LiveEditStyleText statementText)
+	{
+		this.typeOfElement=typeOfElement;
+		if(!(typeOfElement.toUpperCase().equals("DRIVE"))&&(typeOfElement.toUpperCase().equals("COMMAND"))){
+			createSShell(statementText);
+		}
+		else{
+			createDriverCommandSShell(statementText);
+		}
+//		theShell.setSize(new Point(282, 184));
+//		GridLayout gridLayout = new GridLayout();
+//		gridLayout.numColumns = 1;
+//		//theShell.setLayout(gridLayout);
+//		
+//		GridData gridData1 = new GridData();
+//		gridData1.grabExcessHorizontalSpace = true;
+//		gridData1.grabExcessVerticalSpace = false;
+//		GridData gridData = new GridData();
+//		gridData.grabExcessHorizontalSpace = true;
+//		gridData.grabExcessVerticalSpace = true;
+//		
+//		
+//		
+//		Text textToSearch = new Text(theShell, SWT.BORDER);
+//		textToSearch.setLayoutData(gridData1);
+//		List listToSearch = new List(theShell, SWT.NONE);
+//		listToSearch.setLayoutData(gridData);
+//		
+		
+		
+		
+	    
+	}
+	
+	/**
+	 * 
+	 * @param statementText
+	 */
+	private void createDriverCommandSShell(final LiveEditStyleText statementText) {
+		GridData gridData1 = new GridData();
+		gridData1.grabExcessHorizontalSpace = false;
+		gridData1.horizontalAlignment = GridData.FILL;
+		//gridData1.verticalAlignment = GridData.CENTER;
+		gridData1.grabExcessVerticalSpace = true;
+		GridData gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalAlignment = GridData.FILL;
+		gridData.verticalAlignment = GridData.FILL;
+		gridData.grabExcessVerticalSpace = true;
+		
+		GridData gridData23 = new GridData();
+		gridData23.horizontalAlignment = GridData.FILL;
+		gridData23.verticalAlignment = GridData.CENTER;
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 2;
+		theShell = new Shell(Display.getDefault());
+		theShell.setText("Data-Management-Activity properties");
+		//theShell.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+		theShell.setLayout(gridLayout);
+		theShell.setSize(new Point(320, 254));
+		
+		
+		
+		textToSearch = new Text(theShell, SWT.BORDER);
+		textToSearch.addModifyListener(new ModifyListener() {
+			
+			@Override
+			public void modifyText(ModifyEvent e) {
+//				System.out.print(textToSearch.getText());
+				searchListForResults(textToSearch.getText());
+			}
+
+		
+			
+		});	
+		
+		Button addToStatement=new Button(theShell, SWT.NONE);
+		addToStatement.setText("Insert to statement");
+		addToStatement.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				statementText.append(" "+listToSearch.getItems()[listToSearch.getSelectionIndex()]);
+				
+				closeWindow();
+				setWindowIsOpen(false);
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				statementText.append(" "+listToSearch.getItems()[listToSearch.getSelectionIndex()]);
+			}
+		});
+		//textToSearch.setLayoutData(gridData);
+		
+		listToSearch = new List(theShell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		//listToSearch.setItems((String[]) arrayOfElements.toArray());
+		listToSearch.setLayoutData(gridData);
+		listToSearch.addListener(SWT.MouseDoubleClick, new Listener() {
+			
+			@Override
+			public void handleEvent(Event event) {
+				statementText.append(" "+listToSearch.getItems()[listToSearch.getSelectionIndex()]);				
+			}
+		});
+		
+		listToSearch = new List(theShell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		//listToSearch.setItems((String[]) arrayOfElements.toArray());
+		listToSearch.setLayoutData(gridData);
+		listToSearch.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			   if(typeOfElement.toUpperCase().equals("COMMAND")){
+				loadInformationCommand(listToSearch.getItems()[listToSearch.getSelectionIndex()]);
+			   }
+			   if(typeOfElement.toUpperCase().equals("DRIVE")){
+					loadInformationDrive(listToSearch.getItems()[listToSearch.getSelectionIndex()]);
+				}
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+		});
+
+		infoTextBox=new Text(theShell, SWT.BORDER);
+		//infoTextBox.setSize(width, height)
+		
+	}
+	
+	
+	private void createSShell(final LiveEditStyleText statementText) {
+		GridData gridData1 = new GridData();
+		gridData1.grabExcessHorizontalSpace = false;
+		gridData1.horizontalAlignment = GridData.FILL;
+		//gridData1.verticalAlignment = GridData.CENTER;
+		gridData1.grabExcessVerticalSpace = true;
+		GridData gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalAlignment = GridData.FILL;
+		gridData.verticalAlignment = GridData.FILL;
+		gridData.grabExcessVerticalSpace = true;
+		
+		GridData gridData23 = new GridData();
+		gridData23.horizontalAlignment = GridData.FILL;
+		gridData23.verticalAlignment = GridData.CENTER;
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 1;
+		theShell = new Shell(Display.getDefault());
+		theShell.setText("Data-Management-Activity properties");
+		//theShell.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+		theShell.setLayout(gridLayout);
+		theShell.setSize(new Point(320, 254));
+		
+		
+		
+		textToSearch = new Text(theShell, SWT.BORDER);
+		textToSearch.addModifyListener(new ModifyListener() {
+			
+			@Override
+			public void modifyText(ModifyEvent e) {
+//				System.out.print(textToSearch.getText());
+				searchListForResults(textToSearch.getText());
+			}
+		});	
+		
+		Button addToStatement=new Button(theShell, SWT.NONE);
+		addToStatement.setText("Insert to statement");
+		addToStatement.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				statementText.append(" "+listToSearch.getItems()[listToSearch.getSelectionIndex()]);
+				
+				closeWindow();
+				setWindowIsOpen(false);
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				statementText.append(" "+listToSearch.getItems()[listToSearch.getSelectionIndex()]);
+			}
+		});
+		//textToSearch.setLayoutData(gridData);
+		
+		listToSearch = new List(theShell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		//listToSearch.setItems((String[]) arrayOfElements.toArray());
+		listToSearch.setLayoutData(gridData);
+		listToSearch.addListener(SWT.MouseDoubleClick, new Listener() {
+			
+			@Override
+			public void handleEvent(Event event) {
+				statementText.append(" "+listToSearch.getItems()[listToSearch.getSelectionIndex()]);				
+			}
+		});
+		
+		
+		
+		
+	}
+	
+	/**
+	 * for loading the info of a Driver
+	 * into the text box
+	 * @param string
+	 */
+	private void loadInformationDrive(String searchedString) {
+		FileSystemElement tmpElement;
+		infoTextBox.setText("");
+		for(int i=0;i<listOfFSysElements.size();i++){
+			
+			tmpElement=listOfFSysElements.get(i);
+			if(tmpElement.getDriveName().equals(searchedString)){
+				infoTextBox.setText(tmpElement.getDrive_info());
+			}
+			//listColumns.add(tmpTableObjekt.);
+		}
+		
+	}
+	/**
+	 * for loading the info of a Command
+	 * into the text box
+	 * @param string
+	 */
+	private void loadInformationCommand(String searchedString) {
+		FileSystemElement tmpElement;
+		infoTextBox.setText("");
+		for(int i=0;i<listOfFSysElements.size();i++){
+			
+			tmpElement=listOfFSysElements.get(i);
+			if(tmpElement.getCommandName().equals(searchedString)){
+				infoTextBox.setText(tmpElement.getCommandDescription());
+			}
+			//listColumns.add(tmpTableObjekt.);
+		}
+		
+	}
+	
+//	
+	
+	/**
+	 * search live for results in the list
+	 * @param text
+	 */
+	private void searchListForResults(String text) {
+		//arrayOfElements= listToSearch.getItems();
+		listToSearch.removeAll();
+		for(int i=0;i<arrayOfElements.size();i++){
+			if(arrayOfElements.get(i).toLowerCase().contains(text.toLowerCase())){
+				listToSearch.add(arrayOfElements.get(i));
+			}
+		}
+	}
+	
+	
+	/**
+	 * Open window.
+	 */
+	public void openWindow(){
+		theShell.open();
+	}
+	
+	/**
+	 * Close window.
+	 */
+	public void closeWindow(){
+		theShell.close();
+	}
+	
+	/**
+	 * Sets the text.
+	 * 
+	 * @param string
+	 *            the new text
+	 */
+	public void setText(String string) {
+		theShell.setText(string);
+	}
+	
+	/**
+	 * Load values from db.
+	 */
+	public void loadValuesFromDB(){
+		
+		
+		arrayOfElements.add("aaaaa");
+		arrayOfElements.add("abbb");
+		arrayOfElements.add("bbbba");
+		arrayOfElements.add("aaccc");
+		arrayOfElements.add("cccbbb");
+		arrayOfElements.add("ddccbbc");
+		arrayOfElements.add("aa");
+		arrayOfElements.add("sdfsdf");
+		arrayOfElements.add("zzzzzz");
+		arrayOfElements.add("aaa");
+	}
+//	public void setSize(int width,int height) {
+//		theShell.setSize(width, height);
+//	}
+
+	/**
+ * for adding the tables names from the DB.
+ */
+	public void loadDataToFSysElementList(String typeOfElement,DataSource dataSource) {
+		
+		MetaDataXMLParser metaDataXMLParser_Objekt=new MetaDataXMLParser();
+		metaDataXMLParser_Objekt.loadFileSystemElementsFromDB(dataSource);
+		
+		if(typeOfElement.toUpperCase().equals("COMMAND")){
+			listOfFSysElements=metaDataXMLParser_Objekt.getListOfFileSysElements_command();
+			for(int i=0;i<listOfFSysElements.size();i++){
+				listToSearch.add(listOfFSysElements.get(i).getCommandName());
+				arrayOfElements.add(listOfFSysElements.get(i).getCommandName());
+			}
+		}
+		if(typeOfElement.toUpperCase().equals("FOLDER")){
+			listOfFSysElements=metaDataXMLParser_Objekt.getListOfFileSysElements_folder();
+			for(int i=0;i<listOfFSysElements.size();i++){
+				listToSearch.add(listOfFSysElements.get(i).getFolderName());
+				arrayOfElements.add(listOfFSysElements.get(i).getFolderName());
+			}
+		}
+		if(typeOfElement.toUpperCase().equals("DRIVE")){
+			listOfFSysElements=metaDataXMLParser_Objekt.getListOfFileSysElements_drive();
+			for(int i=0;i<listOfFSysElements.size();i++){
+				listToSearch.add(listOfFSysElements.get(i).getDriveName());
+				arrayOfElements.add(listOfFSysElements.get(i).getDriveName());
+			}
+		}
+		
+		if(typeOfElement.toUpperCase().equals("FILE")){
+			listOfFSysElements=metaDataXMLParser_Objekt.getListOfFileSysElements_file();
+			for(int i=0;i<listOfFSysElements.size();i++){
+				listToSearch.add(listOfFSysElements.get(i).getFileName());
+				arrayOfElements.add(listOfFSysElements.get(i).getFileName());
+			}
+		}
+		
+		
+		
+//		arrayOfElements.add("aaaaa");
+//		arrayOfElements.add("abbb");
+//		arrayOfElements.add("bbbba");
+//		arrayOfElements.add("aaccc");
+//		arrayOfElements.add("cccbbb");
+//		arrayOfElements.add("ddccbbc");
+//		arrayOfElements.add("aa");
+//		arrayOfElements.add("sdfsdf");
+//		arrayOfElements.add("zzzzzz");
+//		arrayOfElements.add("aaa");
+	}
+	
+	/**
+	 * for inserting the Bpel-Variables into the List.
+	 */
+	public void loadBPELVariables(java.util.List<String> listOfBPELVariablesAsStrings) {
+		/*
+		 * Hallo,
+		du kannst jetzt in den PropertySections mit 
+		 *VariableUtils.getUseableVariables(getProcess()) *eine Liste mit allen 
+		Variablennamen, die entweder einen SimpleType (string, boolean, ...) 
+		oder den ContainerReferenceType (schema+tabelle) haben, abrufen.
+		Die Namen haben dabei schon die Notation, die wir vorher besprochen 
+		haben, also alle SimpleType Variablennamen sind umgeben von # (z.B. 
+		#simpleVariable#) und alle ContainerReferenceType Variablennamen sind 
+		umgeben von [] (z.B. [containerRefType] ).
+
+		Gruﬂ,
+		Michael
+		 */
+		
+//		arrayOfElements.add("BPEL_Variable1");
+//		arrayOfElements.add("BPEL_Variable2");
+//		arrayOfElements.add("BPEL_Variable3");
+//		arrayOfElements.add("BPEL_Variable4");
+//		arrayOfElements.add("BPEL_Variable5");
+//		arrayOfElements.add("BPEL_Variable6");
+//		arrayOfElements.add("BPEL_Variable7");
+//		arrayOfElements.add("BPEL_Variable8");
+		
+	}
+	
+	public ArrayList<String> parseXMLElements(){
+		
+		
+		
+		return arrayOfElements;
+		
+	}
+	
+	/**
+	 * 
+	 * @param findDataSourceByName
+	 * @return
+	 */
+	public ArrayList<DBTable> getTablesFromDB(DataSource findDataSourceByName) {
+		MetaDataXMLParser metaDataXMLParser_Objekt=new MetaDataXMLParser();
+		ArrayList<DBTable> listOfTables= metaDataXMLParser_Objekt.loadTablesFromDB(findDataSourceByName);
+		
+		return listOfTables;
+	}
+	
+	/**
+	 * 
+	 */
+}
