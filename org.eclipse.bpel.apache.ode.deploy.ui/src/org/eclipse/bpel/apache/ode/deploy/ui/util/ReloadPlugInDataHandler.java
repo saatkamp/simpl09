32abc92d175a2682ed9dce9b2372ed79917d6169
michael.schneidt <michael.schneidt@arcor.de>
2010-10-27 16:09:46 +0000

    resource framework version

---
+++ org.eclipse.bpel.apache.ode.deploy.ui/src/org/eclipse/bpel/apache/ode/deploy/ui/util/ReloadPlugInDataHandler.java
@@ -0,0 +1,36 @@
+/**
+ * <b>Purpose:</b> This handler is used to reload the necessary Plug-In data from the SIMPL Core, if it is available.<br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b>  Licensed under the Apache License, Version 2.0. http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de> <br>
+ * @version $Id: BPELDMReloadDataHandler.java 1202 2010-04-27 16:31:01Z hahnml@t-online.de $ <br>
+ * @link http://code.google.com/p/simpl09/
+ *
+ */
+package org.eclipse.bpel.apache.ode.deploy.ui.util;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ReloadPlugInDataHandler extends AbstractHandler {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		try {
+			//Laden die Daten des SIMPL Cores neu
+			SIMPLCoreMetaData.init();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+}
