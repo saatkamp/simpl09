f7ede075713beb96debaac8f320d9195fbfd103a
michael.schneidt <michael.schneidt@arcor.de>
2010-10-27 16:45:59 +0000

    resource framework version

---
+++ org.eclipse.simpl.communication/src/org/eclipse/simpl/communication/preferences/SIMPLCorePreferencePage.java
@@ -0,0 +1,28 @@
+package org.eclipse.simpl.communication.preferences;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.simpl.communication.CommunicationPlugIn;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class SIMPLCorePreferencePage extends FieldEditorPreferencePage implements
+		IWorkbenchPreferencePage {
+
+	public SIMPLCorePreferencePage() {
+		super(GRID);
+
+	}
+
+	public void createFieldEditors() {
+		addField(new StringFieldEditor("SIMPL_CORE_DSS_ADDRESS", "SIMPL Datasource Service address:",
+				getFieldEditorParent()));
+		addField(new StringFieldEditor("SIMPL_CORE_AS_ADDRESS", "SIMPL Administration Service address:",
+				getFieldEditorParent()));
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(CommunicationPlugIn.getDefault().getPreferenceStore());
+	}
+}
