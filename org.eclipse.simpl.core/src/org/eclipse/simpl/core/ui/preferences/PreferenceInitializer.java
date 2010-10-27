5a27341e723cfcd77c79a4d5e442b1fb890ef973
michael.schneidt <michael.schneidt@arcor.de>
2010-10-27 16:56:51 +0000

    cleaned

--- org.eclipse.simpl.core/src/org/eclipse/simpl/core/ui/preferences/PreferenceInitializer.java
+++ org.eclipse.simpl.core/src/org/eclipse/simpl/core/ui/preferences/PreferenceInitializer.java
@@ -1,8 +1,6 @@
 package org.eclipse.simpl.core.ui.preferences;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.core.runtime.preferences.DefaultScope;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
