caa0c8513ce79136a114c500e352a29617522ba4
michael.schneidt <michael.schneidt@arcor.de>
2011-02-14 16:16:55 +0000

    - deployment simpl-tomcat.zip updated - removed datasource service from rmweb-config

--- org.simpl.core/simpl-tomcat-readme.txt
+++ org.simpl.core/simpl-tomcat-readme.txt
@@ -1,25 +1,30 @@
 = Content =
 SIMPL Core
-SIMPL Core Plugins
+SIMPL Core Plug-Ins
 SIMPL ODE Extensions
-SIMPL RRS
+SIMPL RRS (Reference Resolution System)
 SIMPL RRS Transformation Service
 SIMPL Resource Management
 SIMPL Resource Management Web Interface
 
 = Requirements =
-Apache Tomcat 5.5/6.0
+Apache Tomcat 5.5/6.0 (http://tomcat.apache.org/)
+PostgreSQL 8.4 (http://www.postgresql.org/)
 
 = Installation =
 Extract the simpl-tomcat.zip to the Tomcat root directory (/Tomcat x.x/).
 
+= Configuration =
+Resource Management: webapps\axis2\WEB-INF\conf\simpl-resource-management-config.xml
+Resource Management Webinterface: webapps\rmweb\WEB-INF\conf\rmweb-config.xml
+RRS: webapps\axis2\WEB-INF\conf\rrs-config.xml
+
 = Login =
-The user for Apache Tomcat and thus the resource management web interface is "admin", no password is set.
+The user for Apache Tomcat and thus the Resource Management Web Interface is "admin", no password is set.
 
 = Links =
-SIMPL Core Web Service List: http://localhost:8080/ode/services/listServices
-SIMPL RRS Web Service List: http://localhost:8080/axis2/services/listServices
-SIMPL Resource Management: http://localhost:8080/axis2/services/ResourceManagementService.ResourceManagementPort?wsdl
+SIMPL Core Web Services: http://localhost:8080/ode/services/listServices
+SIMPL RRS and Resource Management Web Services: http://localhost:8080/axis2/services/listServices
 SIMPL Resource Management Web Interface: http://localhost:8080/rmweb
 
 = Problems =
--- org.simpl.core/simpl-tomcat.bat
+++ org.simpl.core/simpl-tomcat.bat
@@ -4,9 +4,8 @@ REM in Apache Tomcat to a .zip file.
 REM 
 REM PARAMETERS
 REM   %1: tomcat root folder
-REM   %2: eclipse workspace folder
-REM   %3: folder to consolidate the deployment files (must exist)
-REM   %4: local svn folder where to put the simpl-tomcat.zip
+REM   %2: folder to consolidate the deployment files (must exist)
+REM   %3: 7-Zip executable
 REM
 REM NOTE
 REM   Directories containing spaces have to be passed in short form e.g. C:\TOMCAT~1.0 instead of C:\Tomcat 6.0
@@ -14,58 +13,51 @@ REM
 REM ECLIPSE EXTERNAL TOOLS EXAMPLE
 REM   Location: ${workspace_loc:/org.simpl.core/simpl-tomcat.bat}
 REM   Working Directory: ${workspace_loc:/org.simpl.core}
-REM   Arguments: C:\TOMCAT~1.0 C:\eclipse\workspace C:\SIMPL\deployment C:\SIMPL\SVN\deployment\simpl-tomcat.zip)
+REM   Arguments: C:\Tomcat6.0 ${workspace_loc:/org.simpl.core}/deployment C:\Programme\7-Zip\7z.exe
 
 @echo off
-REM create directory structure in %3
+REM create directory structure in %2
 @echo on
-md "%3\conf"
-md "%3\common"
-md "%3\common\lib"
-md "%3\lib"
-md "%3\webapps"
-md "%3\webapps\juddiv3"
-md "%3\webapps\juddiweb"
-md "%3\webapps\ode"
-md "%3\webapps\axis2"
+md "%2\conf"
+md "%2\common"
+md "%2\common\lib"
+md "%2\lib"
+md "%2\webapps"
+md "%2\webapps\rmweb"
+md "%2\webapps\ode"
+md "%2\webapps\axis2"
 
 @echo off
-REM copy all files to %3
+REM copy all files to %2
 @echo on
-REM copy "%1\webapps\ode\WEB-INF\servicejars\simpl-core-webservices.jar" "%3\webapps\ode\WEB-INF\servicejars"
-REM copy "%1\webapps\ode\WEB-INF\lib\simpl-core.jar" "%3\webapps\ode\WEB-INF\lib"
-REM copy "%1\webapps\ode\WEB-INF\lib\simpl-core-plugins.jar" "%3\webapps\ode\WEB-INF\lib"
-REM copy "%1\webapps\ode\WEB-INF\lib\simpl-uddi-client.jar" "%3\webapps\ode\WEB-INF\lib"
-REM copy "%1\webapps\ode\WEB-INF\lib\simpl-ode-extension.jar" "%3\webapps\ode\WEB-INF\lib"
-REM copy "%1\webapps\ode\WEB-INF\lib\tools.jar" "%3\webapps\ode\WEB-INF\lib"
-REM copy "%1\webapps\ode\WEB-INF\conf\axis2.xml" "%3\webapps\ode\WEB-INF\conf"
-
-copy "%1\webapps\ode\WEB-INF\lib\derby-10.4.1.3.jar" "%3\common\lib"
-copy "%1\webapps\ode\WEB-INF\lib\derby-10.4.1.3.jar" "%3\lib"
-xcopy "%1\webapps\juddiv3\*.*" "%3\webapps\juddiv3" /s
-xcopy "%1\webapps\ode\*.*" "%3\webapps\ode" /s
-xcopy "%1\webapps\axis2\*.*" "%3\webapps\axis2" /s
-xcopy "%1\webapps\juddiweb\*.*" "%3\webapps\juddiweb" /s
-
-copy "%2\org.simpl.core\lib\*.*" "%3\webapps\ode\WEB-INF\lib"
-copy "%2\org.simpl.core\log4j.properties" "%3\"
-copy "%2\org.simpl.core\simpl-core-config.xml" "%3\webapps\ode\WEB-INF\conf"
-copy "%2\org.apache.ode.simpl.ea\ode-axis2.properties" "%3\webapps\ode\WEB-INF\conf"
-copy "%2\org.simpl.core\readme.txt" "%3\"
-copy "%1\conf\tomcat-users.xml" "%3\conf"
-copy "%4\simpl-rrs.aar" "%3\webapps\ode\WEB-INF\services"
-copy "%4\simpl-rrs-transformation.aar" "%3\webapps\ode\WEB-INF\services"
+copy "%1\webapps\ode\WEB-INF\lib\derby-10.4.1.3.jar" "%2\lib"
+copy "%1\webapps\ode\WEB-INF\lib\derby-10.4.1.3.jar" "%2\common\lib"
+copy "%1\conf\tomcat-users.xml" "%2\conf"
+xcopy "%1\webapps\ode\*.*" "%2\webapps\ode" /E
+xcopy "%1\webapps\axis2\*.*" "%2\webapps\axis2" /E
+xcopy "%1\webapps\rmweb\*.*" "%2\webapps\rmweb" /E
+xcopy "%1\simplDB\*.*" "%2\simplDB\" /E
+copy "%2\..\log4j.properties" "%2"
+copy "%2\..\simpl-tomcat-readme.txt" "%2"
 
 @echo off
-REM create simpl-tomcat.zip in %4
+REM create simpl-tomcat.zip in %2
 @echo on
-C:\Programme\7-Zip\7z.exe a -r "%4\simpl-tomcat.zip" "%3\*.*"
+"%3" a -r "%2\simpl-tomcat.zip" "%2\conf" "%2\common" "%2\lib" "%2\webapps" "%2\simplDB" "%2\log4j.properties" "%2\simpl-tomcat-readme.txt"
 
 @echo off
-REM clean %3
+REM clean %2
 @echo on
-del "%3\*.*" /s /q
-rd "%3\webapps" /s /q
-rd "%3\conf" /s /q
-rd "%3\common" /s /q
-rd "%3\lib" /s /q
+del "%2\conf" /s /q
+del "%2\common" /s /q
+del "%2\lib" /s /q
+del "%2\webapps" /s /q
+del "%2\simplDB" /s /q
+del "%2\log4j.properties"
+del "%2\simpl-tomcat-readme.txt"
+
+rd "%2\conf" /s /q
+rd "%2\common" /s /q
+rd "%2\lib" /s /q
+rd "%2\webapps" /s /q
+rd "%2\simplDB" /s /q
