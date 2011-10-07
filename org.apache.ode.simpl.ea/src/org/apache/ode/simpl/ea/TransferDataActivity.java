87882848a23a0a172f917a12a492e24e5d7f94e7
michael.schneidt <michael.schneidt@arcor.de>
2011-10-07 09:34:08 +0000

    branches/saristidou

---
+++ org.apache.ode.simpl.ea/src/org/apache/ode/simpl/ea/TransferDataActivity.java
@@ -0,0 +1,107 @@
+package org.apache.ode.simpl.ea;
+
+import java.util.Map;
+
+import org.apache.ode.bpel.common.FaultException;
+import org.apache.ode.bpel.evt.ActivityFailureEvent;
+import org.apache.ode.bpel.rtrep.common.extension.ExtensionContext;
+import org.apache.ode.bpel.rtrep.v2.OScope.Variable;
+import org.apache.ode.simpl.ea.util.DataSourceUtils;
+import org.apache.ode.simpl.ea.util.StatementUtils;
+import org.simpl.core.SIMPLCoreInterface;
+import org.simpl.core.services.SIMPLCoreService;
+import org.simpl.resource.management.data.DataSource;
+import org.simpl.resource.management.data.LateBinding;
+import org.w3c.dom.Element;
+
+import commonj.sdo.DataObject;
+
+/**
+ * <b>Purpose:</b> <br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b> Licensed under the Apache License, Version 2.0.
+ * http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de> <br>
+ * @version $Id$ <br>
+ * @link http://code.google.com/p/simpl09/
+ * 
+ */
+public class TransferDataActivity extends DataManagementActivity {
+  @Override
+  protected void runSync(ExtensionContext context, Element element) throws FaultException {
+    // ScopeEvent DMStarted = new DMStarted();
+    // context.getInternalInstance().sendEvent(DMStarted);
+
+    // Load all attribute values from the activity.
+    loadSIMPLAttributes(context, element);
+
+    // Load all specific attribute values from the TransferActivity.
+    String targetDsIdentifier = element.getAttribute("targetDsIdentifier");
+    String targetDsContainer = element.getAttribute("targetDsContainer");
+
+    if (targetDsContainer.contains("[") || targetDsContainer.contains("#")) {
+      // targetDsContainer enthält eine BPEL-Variable als Referenz
+      Map<String, Variable> variables = null;
+      try {
+        variables = context.getVisibleVariables();
+      } catch (FaultException e) {
+        ActivityFailureEvent event = new ActivityFailureEvent(e.toString());
+        event.setActivityName(context.getActivityName());
+        event.setActivityId(context.getOActivity().getId());
+        event.setActivityType("TransferDataActivity");
+        event.setScopeName(context.getOActivity().getParent().name);
+        event.setScopeId(0L);
+        event.setScopeDeclerationId(context.getOActivity().getParent().getId());
+
+        context.getInternalInstance().sendEvent(event);
+      }
+
+      targetDsContainer = String.valueOf(StatementUtils.resolveVariable(context,
+          variables, targetDsContainer));
+    }
+
+    DataSource dsFrom = DataSourceUtils.getDataSource(context, getDsIdentifier());
+    DataSource dsTo = DataSourceUtils.getDataSource(context, targetDsIdentifier);
+    LateBinding lb = DataSourceUtils.getLateBinding(context, getDsIdentifier());
+
+    SIMPLCoreInterface simplCoreService = SIMPLCoreService.getInstance().getService();
+
+    try {
+      if (!targetDsIdentifier.equals("") || !targetDsContainer.equals("")) {
+        DataObject dataObject = simplCoreService.retrieveData(dsFrom,
+            getDsStatement(context), lb);
+
+        if (dataObject == null) {
+          this.successfulExecution = false;
+        } else {
+          this.successfulExecution = simplCoreService.writeDataBack(dsTo, dataObject,
+              targetDsContainer, null);
+        }
+      }
+
+      if (!this.successfulExecution) {
+        ActivityFailureEvent event = new ActivityFailureEvent();
+        event.setActivityName(context.getActivityName());
+        event.setActivityId(context.getOActivity().getId());
+        event.setActivityType("TransferDataActivity");
+        event.setScopeName(context.getOActivity().getParent().name);
+        event.setScopeId(0L);
+        event.setScopeDeclerationId(context.getOActivity().getParent().getId());
+        context.getInternalInstance().sendEvent(event);
+        context.completeWithFault(new Throwable("SIMPL Exception"));
+      }
+    } catch (Exception e) {
+      ActivityFailureEvent event = new ActivityFailureEvent(e.toString());
+      event.setActivityName(context.getActivityName());
+      event.setActivityId(context.getOActivity().getId());
+      event.setActivityType("TransferDataActivity");
+      event.setScopeName(context.getOActivity().getParent().name);
+      event.setScopeId(0L);
+      event.setScopeDeclerationId(context.getOActivity().getParent().getId());
+
+      context.getInternalInstance().sendEvent(event);
+    }
+  }
+}
---
+++ org.apache.ode.simpl.ea/src/org/apache/ode/simpl/ea/WriteDataBackActivity.java
@@ -0,0 +1,107 @@
+package org.apache.ode.simpl.ea;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.ode.bpel.common.FaultException;
+import org.apache.ode.bpel.evt.ActivityFailureEvent;
+import org.apache.ode.bpel.rtrep.common.extension.ExtensionContext;
+import org.apache.ode.bpel.rtrep.v2.OScope.Variable;
+import org.apache.ode.simpl.ea.util.DataSourceUtils;
+import org.simpl.core.SIMPLCoreInterface;
+import org.simpl.core.services.SIMPLCoreService;
+import org.simpl.resource.management.ResourceManagement;
+import org.simpl.resource.management.data.DataSource;
+import org.simpl.resource.management.data.LateBinding;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import commonj.sdo.DataObject;
+import commonj.sdo.helper.XMLDocument;
+import commonj.sdo.helper.XMLHelper;
+import commonj.sdo.helper.XSDHelper;
+
+public class WriteDataBackActivity extends DataManagementActivity {
+  @Override
+  protected void runSync(ExtensionContext context, Element element) throws FaultException {
+    // ScopeEvent DMStarted = new DMStarted();
+    // context.getInternalInstance().sendEvent(DMStarted);
+
+    // Load all attribute values from the activity.
+    loadSIMPLAttributes(context, element);
+
+    // Load all specific attribute values from the WriteDataBackActivity.
+    String writeTarget = element.getAttribute("writeTarget");
+    Attr dataVarAttr = element.getAttributeNode("dataVariable");
+    String dataVariableName = dataVarAttr.getValue();
+
+    DataSource ds = DataSourceUtils.getDataSource(context, getDsIdentifier());
+    LateBinding lb = DataSourceUtils.getLateBinding(context, getDsIdentifier());
+
+    SIMPLCoreInterface simplCoreService = SIMPLCoreService.getInstance().getService();
+
+    try {
+      Node value = null;
+      Variable variable = context.getVisibleVariables().get(dataVariableName);
+
+      if (variable != null) {
+        value = context.readVariable(variable);
+      }
+
+      // get variable content as xml string
+      Source source = new DOMSource(value);
+      StringWriter stringWriter = new StringWriter();
+      Result result = new StreamResult(stringWriter);
+      TransformerFactory factory = TransformerFactory.newInstance();
+      Transformer transformer = factory.newTransformer();
+      transformer.transform(source, result);
+      String xmlValue = stringWriter.getBuffer().toString();
+
+      // retrieve the schema file and define it for SDO
+      ResourceManagement rm = new ResourceManagement();
+      String schema = rm.getDataFormatSchema(
+          ds.getConnector().getDataConverter().getWorkflowDataFormat());
+      InputStream schemaInputStream = new ByteArrayInputStream(schema.getBytes());
+      XSDHelper.INSTANCE.define(schemaInputStream, null);
+
+      // convert xml string to SDO
+      XMLDocument xmlDoc = XMLHelper.INSTANCE.load(xmlValue);
+      DataObject sdo = xmlDoc.getRootObject();
+
+      // write data back
+      this.successfulExecution = simplCoreService.writeDataBack(ds, sdo, writeTarget, lb);
+
+      if (!this.successfulExecution) {
+        ActivityFailureEvent event = new ActivityFailureEvent();
+        event.setActivityName(context.getActivityName());
+        event.setActivityId(context.getOActivity().getId());
+        event.setActivityType("WriteDataBackActivity");
+        event.setScopeName(context.getOActivity().getParent().name);
+        event.setScopeId(0L);
+        event.setScopeDeclerationId(context.getOActivity().getParent().getId());
+        context.getInternalInstance().sendEvent(event);
+        context.completeWithFault(new Throwable("SIMPL Exception"));
+      }
+    } catch (Exception e) {
+      ActivityFailureEvent event = new ActivityFailureEvent(e.toString());
+      event.setActivityName(context.getActivityName());
+      event.setActivityId(context.getOActivity().getId());
+      event.setActivityType("WriteDataBackActivity");
+      event.setScopeName(context.getOActivity().getParent().name);
+      event.setScopeId(0L);
+      event.setScopeDeclerationId(context.getOActivity().getParent().getId());
+
+      context.getInternalInstance().sendEvent(event);
+    }
+  }
+
+}
