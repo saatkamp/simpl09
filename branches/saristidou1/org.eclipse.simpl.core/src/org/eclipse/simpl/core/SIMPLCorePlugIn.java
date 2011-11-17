b540a2e1698ffb1cc64e2f7f1f3a4f0c39a75248
michael.schneidt@arcor.de <michael.schneidt@arcor.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2011-11-17 10:49:18 +0000

    new branch

---
+++ branches/saristidou1/org.eclipse.simpl.core/src/org/eclipse/simpl/core/SIMPLCorePlugIn.java
@@ -0,0 +1,66 @@
+package org.eclipse.simpl.core;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class SIMPLCorePlugIn extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.simpl.core";
+
+	// The shared instance
+	private static SIMPLCorePlugIn plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public SIMPLCorePlugIn() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		
+		if (Application.getInstance().isSIMPLCoreAvailable()){
+			Application.getInstance().initApplication();
+			Application.getInstance().setDataLoaded(true);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static SIMPLCorePlugIn getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+}
