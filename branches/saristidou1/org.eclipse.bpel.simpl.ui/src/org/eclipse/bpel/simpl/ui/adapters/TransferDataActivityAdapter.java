78724e46bfbfaa96159f3d24ccf437dc9b769604
michael.schneidt@arcor.de <michael.schneidt@arcor.de@4f467192-6a30-11de-a84c-2f1c77ec4fbd>
2011-11-17 10:48:05 +0000

    new branch

---
+++ branches/saristidou1/org.eclipse.bpel.simpl.ui/src/org/eclipse/bpel/simpl/ui/adapters/TransferDataActivityAdapter.java
@@ -0,0 +1,22 @@
+/**
+ * <b>Purpose:</b> <br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b>  Licensed under the Apache License, Version 2.0. http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de> <br>
+ * @version $Id: CallActivityAdapter.java 1028 2010-03-30 14:33:18Z hahnml@t-online.de $ <br>
+ * @link http://code.google.com/p/simpl09/
+ *
+ */
+package org.eclipse.bpel.simpl.ui.adapters;
+
+
+/**
+ * The Class CallActivityAdapter.
+ * 
+ * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de>
+ */
+public class TransferDataActivityAdapter extends DMActivityAdapter {
+
+}
---
+++ branches/saristidou1/org.eclipse.bpel.simpl.ui/src/org/eclipse/bpel/simpl/ui/adapters/WriteDataBackActivityAdapter.java
@@ -0,0 +1,20 @@
+/**
+ * <b>Purpose:</b> <br>
+ * <b>Description:</b> <br>
+ * <b>Copyright:</b>  Licensed under the Apache License, Version 2.0. http://www.apache.org/licenses/LICENSE-2.0<br>
+ * <b>Company:</b> SIMPL<br>
+ * 
+ * @author Michael Hahn <hahnml@studi.informatik.uni-stuttgart.de> <br>
+ * @version $Id: RetrieveDataActivityAdapter.java 1656 2010-08-18 15:43:55Z michael.schneidt@arcor.de $ <br>
+ * @link http://code.google.com/p/simpl09/
+ *
+ */
+package org.eclipse.bpel.simpl.ui.adapters;
+
+
+/**
+ * The Class WriteDataBackActivityAdapter.
+ */
+public class WriteDataBackActivityAdapter extends DMActivityAdapter {
+
+}
